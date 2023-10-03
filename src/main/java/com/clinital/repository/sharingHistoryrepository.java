2844d65911a8c7bd08803004429855987e83b9bc
solaymanebenhaddi <heddoudi.mohamed@gmail.com>
2023-10-03 00:00:11 +0000

    respository and services for sharing history

--- src/main/java/com/clinital/repository/sharingHistoryrepository.java
+++ src/main/java/com/clinital/repository/sharingHistoryrepository.java
@@ -1,11 +1,14 @@
 package com.clinital.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.clinital.models.Medecin;
 import com.clinital.models.Rendezvous;
 import com.clinital.models.SharingHistory;
 
@@ -14,6 +17,26 @@ public interface sharingHistoryrepository extends JpaRepository<SharingHistory, 
 
     @Modifying
 	@Query(value = "DELETE FROM sharing_history WHERE id = :idshare", nativeQuery = true)
-	void Deletsharehstory(@Param("idshare") Long idshare) throws Exception;
+	public static boolean Deletsharehstory(@Param("idshare") Long idshare) throws Exception{
+        return true;
+    };
+
+    @Query(value = "select * from sharing_history where id_med=?1 and id_patient=?2 ", nativeQuery = true)
+    public List<SharingHistory> findAllSharingHistoryByMedecinIdAndPatientId(Long id_medecin,Long id_patient) throws Exception;
+
+    @Query(value = "select * from sharing_history where id_patient=?1", nativeQuery = true)
+    public List<SharingHistory> findAllSharingHistoryByPatientId(Long id_patient) throws Exception;
+
+    @Query(value = "select m.* from medecins m, sharing_history s where id_med=?1 and id_patient=?2", nativeQuery = true)
+    public List<Medecin> findAllSharingHistoryByMedecinId(Long id_med) throws Exception;
+
+    @Query(value = "", nativeQuery = true)
+    public List<SharingHistory> findAllSharingHistoryByMedecinIdAndUserId(Long id_medecin,Long id_User) throws Exception;
+
+    @Query(value = "", nativeQuery = true)
+    public List<SharingHistory> findAllSharingHistoryByUserId(Long user_id) throws Exception;
+
+    @Query(value = "", nativeQuery = true)
+    public List<SharingHistory> findAllSharingHistoryByDocId(Long id_document) throws Exception;
     
 }
